f41aa7fee02f914fa2f19f0fe4c3ed91089fd5c3
Arnaud Delorme <arnodelorme@gmail.com>
2020-12-17 11:31:14 -1000

    Update eegplugin_neuroscanio.m

--- eegplugin_neuroscanio.m
+++ eegplugin_neuroscanio.m
@@ -30,7 +30,7 @@
 
 function vers = eegplugin_neuroscanio(fig, trystrs, catchstrs)
 
-vers = 'neuroscanio1.3';
+vers = 'neuroscanio1.4';
 if nargin < 3
     error('eegplugin_neuroscanio requires 3 arguments');
 end
